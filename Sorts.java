bb9f1de18bc194b1984177860953f7c23157b2f7
Derek Lao <dlao00@stuy.edu>
2018-12-10 01:14:58 -0500

    successful for both types of sorts for my one test case, and set up exception handling for if the length of the data for each sort is less than 1

--- Sorts.java
+++ Sorts.java
@@ -16,19 +16,27 @@ public class Sorts{
   */
   public static void selectionSort(int[] data){
     int n,i,holder;
-    for(n=0;n<data.length;n++)
+    if(data.length>0)
     {
-      int lowIndex=n;
-      for(i=n;i<data.length;i++)
+      for(n=0;n<data.length;n++)
       {
-        if(data[lowIndex]>data[i])
-        lowIndex=i;
+        int lowIndex=n;
+        for(i=n;i<data.length;i++)
+        {
+          if(data[lowIndex]>data[i])
+          lowIndex=i;
+        }
+        holder=data[n];
+        data[n]=data[lowIndex];
+        data[lowIndex]=holder;
+        //System.out.println("My data is now: "+toString(data));
       }
-      holder=data[n];
-      data[n]=data[lowIndex];
-      data[lowIndex]=holder;
-      //System.out.println("My data is now: "+toString(data));
     }
+    // although this part is unnecessary,
+    // it is done to acknowledge that I know that
+    // the exception is being thrown
+    else
+    throw new ArrayIndexOutOfBoundsException("Your array must have length > 1");
   }
 
 
@@ -39,9 +47,9 @@ public class Sorts{
   public static void bubbleSort(int[] data){
     if (data.length>0)
     {
-      for(int a=data.length;a>0;a++)
+      for(int a=data.length;a>0;a--)
       {
-        for(int i=data.length-1-a,n=0;i>0;i++)
+        for(int i=data.length-1,n=0;i>0;i--)
         {
           if (data[i]<data[i-1])
           {
@@ -53,20 +61,51 @@ public class Sorts{
           {
             continue;
           }
-          System.out.println(data);
+          //System.out.println(data);
         }
+        // System.out.println("My data is now: "+toString(data));
       }
     }
+    // although this part is unnecessary,
+    // it is done to acknowledge that I know that
+    // the exception is being thrown
+    else
+    throw new ArrayIndexOutOfBoundsException();
   }
 
   public static void main(String[] args){
     int[] data1={9,8,7,6,5,4,3,2,1};
+    System.out.println("Original data: "+toString(data1));
     selectionSort(data1);
-    System.out.println("Original data: {9,8,7,6,5,4,3,2,1}");
     System.out.println("After selectionSort: "+toString(data1));
     int[] data2={9,8,7,6,5,4,3,2,1};
+    // int[] data2={2,1};
+    System.out.println("Original data: "+toString(data2));
     bubbleSort(data2);
-    System.out.println("Original data: {9,8,7,6,5,4,3,2,1}");
     System.out.println("After bubbleSort: "+toString(data2));
+    try
+    {
+      int[] data3={};
+      System.out.println("Original data: "+toString(data3));
+      selectionSort(data3);
+      System.out.println("After selectionSort: "+toString(data3));
+    }
+    catch(ArrayIndexOutOfBoundsException e)
+    {
+      System.out.println("Error: can't do that!");
+      e.printStackTrace();
+    }
+    try
+    {
+      int[] data4={};
+      System.out.println("Original data: "+toString(data4));
+      bubbleSort(data4);
+      System.out.println("After selectionSort: "+toString(data4));
+    }
+    catch(ArrayIndexOutOfBoundsException e)
+    {
+      System.out.println("Error: can't do that!");
+      e.printStackTrace();
+    }
   }
 }
